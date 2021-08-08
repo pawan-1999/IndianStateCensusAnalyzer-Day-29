c98d52c4b3ece584dfa2b74e9b86421c5998624f
pawan-1999 <guptapawan767@gmail.com>
2021-08-08 22:46:45 +0530

    Add project files.

---
+++ IndianStateCensusAnalyzer-Day-29/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyzer_Day_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
